9561212366d2cd7cb0ee71987af3c2ce45319a65
ma1598 <ma1598@students.necsd.net>
2022-07-07 14:35:16 -0400

    newest push

--- client/public/js/feed.js
+++ client/public/js/feed.js
@@ -17,9 +17,9 @@ function feedItem(title, body, linkUrl, imageUrl) {
     this.imageUrl = imageUrl;
 }
 
-let story1 = new feedItem('Eren Jaeger', 'Is a character in Attack On Titan, he can be seen below.', 'https://www.marist.edu/', 'images/chaderen.png');
-let story2 = new feedItem('Sus Red Crewmate', 'This man is very sus, the other crewmates susect he could actually be an imposter, who killed the white crewmate.He can also be seen in the to right corner, as the logo of this site.', 'https://www.marist.edu/', 'images/erenjaeger.png');
-let story3 = new feedItem('This is Dead White Crewmate', 'He is so dead because he was actually killed by the red imposter, which is what some of the crewmates have susected.', 'https://www.marist.edu/', 'images/eric.png');
+let story1 = new feedItem('<strong>Eren Jaeger</strong>', 'Is a character in Attack On Titan, he can be seen below.', 'https://www.marist.edu/', 'images/chaderen.png');
+let story2 = new feedItem('<strong>Sus Red Crewmate</strong>', 'This man is very sus, the other crewmates susect he could actually be an imposter, who killed the white crewmate.He can also be seen in the to right corner, as the logo of this site.', 'https://www.marist.edu/', 'images/erenjaeger.png');
+let story3 = new feedItem('<strong>This is Dead White Crewmate</strong>', 'He is so dead because he was actually killed by the red imposter, which is what some of the crewmates have susected.', 'https://www.marist.edu/', 'images/eric.png');
 
 var currentStories = [story1, story2, story3];
 
@@ -27,7 +27,7 @@ var currentStories = [story1, story2, story3];
 function displayItem() {
     for (var i=0; i < currentStories.length; i++){
         let item = currentStories[i];
-        document.getElementById("newsfeed").innerHTML += "<a href=" + item.linkUrl + ">(Suspectual link)</a>" + "<div>" + item.title + "<div>" + item.body + "</div>" + "</div>" + "<img class = 'feedImage' src = " + item.imageUrl + ">";
+        document.getElementById("newsfeed").innerHTML += "<a href=" + item.linkUrl + ">(Suspectual link)</a>" + "<div>" + item.title + "<div>" + item.body + "</div>" + "</div>" + "<img class = 'feedImage' src = " + item.imageUrl + "><hr>";
         
     }
 } 
